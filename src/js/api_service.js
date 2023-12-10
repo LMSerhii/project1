62f3e31dffaa896f7aec4992a550cde8a0c4dbb6
KaterynaMishchuk12 <138882300+KaterynaMishchuk12@users.noreply.github.com>
2023-12-10 12:02:19 +0200

    task2 postNewContact

--- src/js/api_service.js
+++ src/js/api_service.js
@@ -4,6 +4,7 @@ import { common } from './common.js';
 async function getData() {
   try {
     const response = await axios.get(`${common.BASE_URL}work/contacts/`);
+    console.log(response);
     return response;
   } catch (error) {
     console.log(error.message);
@@ -12,12 +13,21 @@ async function getData() {
 
 async function dataGetForID(id) {
   try {
-    const response = await axios.get(`${common.BASE_URL}work/contacts/${id}`)
+    const response = await axios.get(`${common.BASE_URL}work/contacts/${id}`);
     return response;
   } catch (error) {
     console.log(error.message);
   }
 }
 
+async function createNewContact(data) {
+  try {   
+    const response = await axios.post(
+      `${common.BASE_URL}work/contacts/`,
+      data
+    );
+    console.log(response);
+  } catch (error) {console.log(error)}
+}
 
-export {getData, dataGetForID}
+export { getData, dataGetForID, createNewContact };
