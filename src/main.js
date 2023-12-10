388c73e045289135fae7cd9a388498644015d6cc
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-10 12:24:25 +0200

    add delete

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import { getData, dataGetForID, createNewContact } from './js/api_service.js'
-
+import {
+  getData,
+  dataGetForID,
+  createNewContact,
+  deleteContacts,
+} from './js/api_service.js';
 
 // console.log(getData());
 
-async function gedDataID(id){
+async function gedDataID(id) {
   try {
-    const result = await dataGetForID(id)
+    const result = await dataGetForID(id);
     console.log(result.data);
     return result.data;
   } catch (error) {
@@ -13,11 +17,22 @@ async function gedDataID(id){
   }
 }
 
-async function addContact(dataUser){
-try{
-  await createNewContact(dataUser)
-} 
-catch(error){console.log(error)}
+async function addContact(dataUser) {
+  try {
+    await createNewContact(dataUser);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+async function clearContact(dataID) {
+  try {
+    const response = await deleteContacts(dataID);
+    console.log(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+clearContact(65);
 
 // addContact({name: "Kateryna", phone: "1234567"});
